1a6c2aba1b5cf637afd4eaec1a44e95c9f0d15c0
juanyanik <juan.yanik6@gmail.com>
2024-10-21 11:09:17 -0300

    .

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "snowflake_database" "demo_db" {
   comment = "Database for Snowflake Terraform demo"
 }
 
-resource "snowflake_schema" "demo_schema" {
+resource "snowflake_schema" "demo_schema_demo_db" {
   database = snowflake_database.demo_db.name
   name     = "DEMO_SCHEMA"
   comment  = "Schema for Snowflake Terraform demo"
@@ -34,7 +34,7 @@ resource "snowflake_database" "dev_db" {
   comment = "Database for Snowflake Terraform demo"
 }
 
-resource "snowflake_schema" "demo_schema" {
+resource "snowflake_schema" "demo_schema_dev_db" {
   database = snowflake_database.dev_db.name
   name     = "DEMO_SCHEMA"
   comment  = "Schema for Snowflake Terraform demo"
@@ -45,7 +45,7 @@ resource "snowflake_database" "prod_db" {
   comment = "Database for Snowflake Terraform demo"
 }
 
-resource "snowflake_schema" "demo_schema" {
+resource "snowflake_schema" "demo_schema_prod_db" {
   database = snowflake_database.prod_db.name
   name     = "DEMO_SCHEMA"
   comment  = "Schema for Snowflake Terraform demo"
