e4fd75907cebd3cc9d23ae420a8fbe1c61292ba1
juanyanik <juan.yanik6@gmail.com>
2024-10-21 11:19:24 -0300

    tf code change

--- main.tf
+++ main.tf
@@ -21,32 +21,56 @@ provider "snowflake" {
 resource "snowflake_database" "demo_db" {
   name    = "DEMO_DB"
   comment = "Database for Snowflake Terraform demo"
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
+resource "snowflake_database" "dev_db" {
+  name    = "DEV_DB"
+  comment = "Database for Snowflake Terraform demo"
+
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
+resource "snowflake_database" "prod_db" {
+  name    = "PROD_DB"
+  comment = "Database for Snowflake Terraform demo"
+
+  lifecycle {
+    ignore_changes = all
+  }
 }
 
 resource "snowflake_schema" "demo_schema_demo_db" {
   database = snowflake_database.demo_db.name
   name     = "DEMO_SCHEMA"
   comment  = "Schema for Snowflake Terraform demo"
-}
 
-resource "snowflake_database" "dev_db" {
-  name    = "DEV_DB"
-  comment = "Database for Snowflake Terraform demo"
+  lifecycle {
+    ignore_changes = all
+  }
 }
 
 resource "snowflake_schema" "demo_schema_dev_db" {
   database = snowflake_database.dev_db.name
   name     = "DEMO_SCHEMA"
   comment  = "Schema for Snowflake Terraform demo"
-}
 
-resource "snowflake_database" "prod_db" {
-  name    = "PROD_DB"
-  comment = "Database for Snowflake Terraform demo"
+  lifecycle {
+    ignore_changes = all
+  }
 }
 
 resource "snowflake_schema" "demo_schema_prod_db" {
   database = snowflake_database.prod_db.name
   name     = "DEMO_SCHEMA"
   comment  = "Schema for Snowflake Terraform demo"
+
+  lifecycle {
+    ignore_changes = all
+  }
 }
